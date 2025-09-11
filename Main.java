7fbbf556d04673c69131fa8f238220387b02d101
Cristian Camilo Echeverri <crisechevrri@gmail.com>
2025-09-10 21:22:35 -0500

    Se agregan comentarios a la apicación

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 public class Main {
+
+    /**
+     * este es el método de entrada de la aplicación
+     * @param args argumentos de línea de comandos
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hola, mundo");
-        int numero = 2;
+        //int numero = 2;
+
     }
 }
